ab141433adcb5fbaaf490d5586cc62b5fcaa1c50
Artem Petlia <pak@servers.com>
2023-03-19 16:47:02 +0200

    add task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,13 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ["Potatoes", "Mushrooms", "Garlic", "Tomatos", "Herbs", "Condiments"];
+
+const ingredientsEl = document.querySelector("#ingredients");
+
+const listOfLi = ingredients.map(item => {
+  const li = document.createElement("li");
+  li.textContent = item;
+  li.classList.add("item");
+
+  return li;
+});
+
+ingredientsEl.append(...listOfLi);
