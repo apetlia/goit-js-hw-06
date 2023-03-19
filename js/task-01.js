3ac9687fead8f24d8b4b6d865d737e9273e57696
Artem Petlia <pak@servers.com>
2023-03-19 15:31:57 +0200

    finish 1 task

---
+++ js/task-01.js
@@ -0,0 +1,12 @@
+const categoriesEl = document.querySelector("#categories");
+const categoriesItems = categoriesEl.children;
+
+console.log(`Number of categories: ${categoriesItems.length}`);
+
+for (let i = 0; i < categoriesItems.length; i += 1) {
+  const item = categoriesItems[i];
+  const category = item.firstElementChild.textContent;
+  const categoryCount = item.lastElementChild.childElementCount;
+  console.log(`Category: ${category}`);
+  console.log(`Elements: ${categoryCount}`);
+}
