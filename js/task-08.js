7784d9cf4249632dad5800cbf167db6c614136cb
Artem Petlia <pak@servers.com>
2023-03-27 18:11:16 +0300

    add task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
+const formRef = document.querySelector(".login-form");
 
+formRef.addEventListener("submit", onFormsubmit);
+
+function onFormsubmit(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (!email.value || !password.value) {
+    alert("Fill all cell");
+    return;
+  }
+
+  const credentials = {
+    email: email.value,
+    password: password.value,
+  };
+
+  console.log(credentials);
+  formRef.reset();
+}
