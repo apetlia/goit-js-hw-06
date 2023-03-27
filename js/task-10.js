8ada425a2eae98fff057f5338c904e70ec024665
Artem Petlia <pak@servers.com>
2023-03-27 20:50:10 +0300

    add task 10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,38 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const inputRef = document.querySelector("#controls input");
+const btnCreateRef = document.querySelector("button[data-create]");
+const btnDestroyRef = document.querySelector("button[data-destroy]");
+const boxesDivRef = document.getElementById("boxes");
+
+btnDestroyRef.addEventListener("click", destroyBoxes);
+btnCreateRef.addEventListener("click", onButtonClick);
+
+function destroyBoxes() {
+  boxesDivRef.innerHTML = "";
+}
+
+function createBoxes(amount) {
+  const initialSize = 30;
+  const listOfNewBoxesElement = [];
+
+  for (let i = 0; i < amount; i += 1) {
+    const size = initialSize + i * 10;
+
+    const box = document.createElement("div");
+    box.style.width = size + "px";
+    box.style.height = size + "px";
+    box.style.backgroundColor = getRandomHexColor();
+
+    listOfNewBoxesElement.push(box);
+  }
+
+  boxesDivRef.append(...listOfNewBoxesElement);
+}
+
+function onButtonClick(event) {
+  const amountBoxes = Number(inputRef.value);
+  createBoxes(amountBoxes);
+}
