8fcc42364adfda91c396a0133a89ac436270d2f7
Artem Petlia <pak@servers.com>
2023-03-27 18:38:57 +0300

    add task 9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const colorSpanRef = document.querySelector(".color");
+const buttonRef = document.querySelector(".change-color");
+
+buttonRef.addEventListener("click", onButtonClick);
+
+function onButtonClick() {
+  const colorHex = getRandomHexColor();
+  colorSpanRef.textContent = colorHex;
+  document.body.style.backgroundColor = colorHex;
+}
